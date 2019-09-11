2f2bcfbfb0f9fea6521223d99f3792bb88fac317
Kirsten Greed <kirsteng@jobtalk.com.au>
2019-09-11 10:15:53 +1000

    Add project files.

---
+++ Solution7.Module/BusinessObjects/Solution7DbContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Data.Entity;
+using System.Data.Common;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.ComponentModel;
+using DevExpress.ExpressApp.EF.Updating;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EF.DesignTime;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+
+namespace Solution7.Module.BusinessObjects {
+	public class Solution7ContextInitializer : DbContextTypesInfoInitializerBase {
+		protected override DbContext CreateDbContext() {
+			DbContextInfo contextInfo = new DbContextInfo(typeof(Solution7DbContext), new DbProviderInfo(providerInvariantName: "System.Data.SqlClient", providerManifestToken: "2008"));
+            return contextInfo.CreateInstance();
+		}
+	}
+	[TypesInfoInitializer(typeof(Solution7ContextInitializer))]
+	public class Solution7DbContext : DbContext {
+		public Solution7DbContext(String connectionString)
+			: base(connectionString) {
+            Console.WriteLine("hi");
+        }
+		public Solution7DbContext(DbConnection connection)
+			: base(connection, false) {
+            Console.WriteLine("hi");
+        }
+		public Solution7DbContext() {
+
+            Console.WriteLine("hi");
+		}
+		public DbSet<ModuleInfo> ModulesInfo { get; set; }
+	    public DbSet<PermissionPolicyRole> Roles { get; set; }
+		public DbSet<PermissionPolicyTypePermissionObject> TypePermissionObjects { get; set; }
+		public DbSet<PermissionPolicyUser> Users { get; set; }
+		public DbSet<ModelDifference> ModelDifferences { get; set; }
+		public DbSet<ModelDifferenceAspect> ModelDifferenceAspects { get; set; }
+	}
+}
---
+++ Solution7.Win/WinApplication.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Win;
+using System.Collections.Generic;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp.Win.Utils;
+using DevExpress.ExpressApp.EF;
+using Solution7.Module.BusinessObjects;
+using System.Data.Common;
+
+namespace Solution7.Win {
+    // For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.Win.WinApplication._members
+    public partial class Solution7WindowsFormsApplication : WinApplication {
+        #region Default XAF configuration options (https://www.devexpress.com/kb=T501418)
+        static Solution7WindowsFormsApplication() {
+            DevExpress.Persistent.Base.PasswordCryptographer.EnableRfc2898 = true;
+            DevExpress.Persistent.Base.PasswordCryptographer.SupportLegacySha512 = false;
+			DevExpress.ExpressApp.Utils.ImageLoader.Instance.UseSvgImages = true;
+        }
+        private void InitializeDefaults() {
+            LinkNewObjectToParentImmediately = false;
+            OptimizedControllersCreation = true;
+            UseLightStyle = true;
+			SplashScreen = new DXSplashScreen(typeof(XafSplashScreen), new DefaultOverlayFormOptions());
+			ExecuteStartupLogicBeforeClosingLogonWindow = true;
+        }
+        #endregion
+        public Solution7WindowsFormsApplication() {
+            InitializeComponent();
+			InitializeDefaults();
+        }
+        protected override void CreateDefaultObjectSpaceProvider(CreateCustomObjectSpaceProviderEventArgs args) {
+			if(args.Connection != null) {
+				args.ObjectSpaceProviders.Add(new EFObjectSpaceProvider(typeof(Solution7DbContext), TypesInfo, null, (DbConnection)args.Connection));
+			}
+			else {
+				args.ObjectSpaceProviders.Add(new EFObjectSpaceProvider(typeof(Solution7DbContext), TypesInfo, null, args.ConnectionString));
+			}
+            args.ObjectSpaceProviders.Add(new NonPersistentObjectSpaceProvider(TypesInfo, null));
+        }
+        private void Solution7WindowsFormsApplication_CustomizeLanguagesList(object sender, CustomizeLanguagesListEventArgs e) {
+            string userLanguageName = System.Threading.Thread.CurrentThread.CurrentUICulture.Name;
+            if(userLanguageName != "en-US" && e.Languages.IndexOf(userLanguageName) == -1) {
+                e.Languages.Add(userLanguageName);
+            }
+        }
+        private void Solution7WindowsFormsApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e) {
+#if EASYTEST
+            e.Updater.Update();
+            e.Handled = true;
+#else
+            if(System.Diagnostics.Debugger.IsAttached) {
+                e.Updater.Update();
+                e.Handled = true;
+            }
+            else {
+				string message = "The application cannot connect to the specified database, " +
+					"because the database doesn't exist, its version is older " +
+					"than that of the application or its schema does not match " +
+					"the ORM data model structure. To avoid this error, use one " +
+					"of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+				if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+					message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+				}
+				throw new InvalidOperationException(message);
+            }
+#endif
+        }
+    }
+}
